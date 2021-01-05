629d4db71d2a30590bc13ffe37f0e1e24564c42e
Povelitel Ilya <nubik7-9-0@mail.ru>
2021-01-05 16:28:25 +0700

    Last PART

--- Lab1_2/Lab3-6/main.cpp
+++ Lab1_2/Lab3-6/main.cpp
@@ -1,84 +1,124 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <algorithm>
 #include <cassert>
+#include <stack>
 #include "Bankomat.h"
 #include "Bank_transition+Bank_withHistory.h"
 #include "Bank_nameAddress.h"
 #include "Node.h"
+#include <time.h>
+#include <map>
 
 using namespace std;
 
-void TestingPolymorphismQuality(Bi_tree Check2)
+void Lab8_Object_Test()
 {
-	const char* namee = "123asd";
-	const char* namee1 = "123asd";
-	const char* namee2 = "123asd";
-	char* nameeee = new char[20];
-	strcpy(nameeee, namee1);
-	char* name2 = new char[20];
-	char* namee23 = new char[20];
-	strcpy(name2, namee);
-	strcpy(namee23, namee2);
-	const char* m = " 123asd,15000,5000";
-	Bankomat bla(nameeee, 15000, 5000);
-	Bank_withHistory address(name2,15000,5000);
-	Bank_name_address bear(namee23,15000,5000);
-	Check2.push(&bla);
-	Check2.push(&address);
-	Check2.push(&bear);
-	if ((address.toString() == m) && (Check2.toString() == m) && (bla.toString() == m ))
+	int a, b;
+	char* SS = new char[20];
+	const char* SSS = "Happiness ";
+	int start, end;
+	stack <Bankomat> myStack;
+	cout << " Start BaNkOmAt stack " << endl;
+	start = clock();
+	strcpy(SS, SSS);
+	for (int g = 0; g < 10000; g++)
 	{
-		cout << " Polymorphism working properly " << endl;
+		a = rand() % 10000;
+		b = rand() % 5000;
+		Bankomat* Afs = new Bankomat(SS, a, b);
+		myStack.push(*Afs);
 	}
-	else
+	end = clock() - start;
+	cout << " Time for creating : " << end << endl;
+	start = clock();
+	for (int i = 1; i < 3; i++)
+		myStack.pop();
+	end = clock() - start;
+	cout << " Time for delete : " << end << endl;
+
+	// Map
+
+	char* SS = new char[20];
+	const char* SSS = "Happiness ";
+	int start, end;
+	multimap<int, Bankomat> myMultiMap;
+	cout << " Start bankomat multimap " << endl;
+	start = clock();
+	strcpy(SS, SSS);
+	for (int i = 0; i < 10000; i++)
 	{
-		cout << "Polymorphism is suck " << endl;
-		assert(&Check2);
+		a = rand() % 10000;
+		b = rand() % 5000;
+		Bankomat* Afs = new Bankomat(SS, a, b);
+		myMultiMap.insert(make_pair(i, Bankomat::add(i, Afs)));
 	}
+
+	end = clock() - start;
+	cout << " Time for creating : " << end << endl;
+
+	map<int, Bankomat> ::iterator it2;
+
+	start = clock();
+
+	it2 = myMultiMap.find(10001);
+
+	end = clock() - start;
+	cout << "Searching time: " << end << endl;
+
+	start = clock();
+	for (int i = 1; i < 10000; i++) myMultiMap.erase(i);
+	end = clock() - start;
+	cout << "Deleting time: " << end << endl;
+
 }
-void TestingTreePushRemove(Bankomat* sir, Bi_tree frien, Bankomat* sir1,Bankomat* sir2)
+
+
+void Lab_8()
 {
-	frien.push(sir);
-	frien.push(sir1);
-	frien.push(sir2);
-	frien.remove(sir2);
-	frien.toString();
-	cout << "  If tree was shown with 2 elements then function of removing and pushing are working correctly " << endl;
+	stack <int> myIntStack;
+	int start, end;
+	start = clock();
+	cout << " Start int stack " << endl;
+	for (int i = 0; i < 10000; i++)
+		myIntStack.push(i);
+	end = clock() - start;
+	cout << " Time for creating : " << end << endl;
+	start = clock();
+	for (int i = 1; i < 10000; i++)
+		myIntStack.pop();
+	end = clock() - start;
+	cout << " Time for delete : " << end << endl;
+
+	multimap<int, int> myIntMap;
+
+	start = clock();
+
+	for (int i = 0; i < 10000; i++) myIntMap.insert(make_pair(i, 10000 - i)); //добавление 10000 элементов
+
+	end = clock() - start;
+	std::cout << "Creating time: " << end << endl;
+
+	map<int, int> ::iterator it;
+
+	start = clock();
+
+	it = myIntMap.find(10001);
+
+	end = clock() - start;
+	std::cout << "Searching time: "<< end << endl;
+
+	start = clock();
+	for (int i = 1; i < 10000; i++) myIntMap.erase(i);
+	end = clock() - start;
+	std::cout << "Deleting time: " << end << endl;
+
 }
-void TestingTreeFind(Bankomat* sir, Bi_tree frien, Bankomat* sir1, Bankomat* sir2)
-{
-	char* Find = new char[20];
-	cin.getline(Find, 20);
-	frien.push(sir);
-	frien.push(sir1);
-	frien.push(sir2);
-	frien.find(Find);
-	cout << "  If tree was shown right object with the same ID, then Find is working correctly " << endl;
-}
+
 
 int main()
 {
-	char* name = new char[20];
-	char* name1 = new char[20];
-	char* name2 = new char[20];
-	char* name3 = new char[20];
-	cout << " Input ID " << endl;
-	cin.getline(name, 20);
-	cout << " Input ID " << endl;
-	cin.getline(name1, 20);
-	cout << " Input ID " << endl;
-	cin.getline(name2, 20);
-	cout << " Input Finding " << endl;
-	cin.getline(name3, 20);
-	Bankomat sir(name,15000, 5000);
-	Bankomat sir1(name1, 15000, 5000);
-	Bankomat sir2(name2, 15000, 5000);
-	Bank_withHistory bla(name3, 13434, 340);
-	Bi_tree frien;
-	TestingPolymorphismQuality(frien);
-	TestingTreePushRemove(&sir,frien,&sir1,&sir2);
-	TestingTreeFind(&sir,frien, &sir1,&sir2);
+	Lab8_Object_Test();
+	Lab_8();
 	return 0;
 }
